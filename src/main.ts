8d44ebaf3c6c2bf2357827ed83aa249963c68b0f
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2025-01-13 03:00:09 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+    origin: '*', // Allow requests from any origin. Replace '*' with specific origins for better security.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Specify allowed HTTP methods.
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Specify allowed headers.
+    credentials: true, // Enable if you need to allow credentials like cookies.
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
