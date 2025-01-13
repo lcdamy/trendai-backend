6cd00b8f3c5a236989ef947592f00818d9de15bd
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2025-01-13 04:08:30 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
 
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
-  console.log(Application is running on: ${await app.getUrl()});
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
